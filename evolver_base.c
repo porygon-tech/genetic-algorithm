d25ecc53d346fb8fb35120bd7b1672853ae293c6
coliflora <49144138+coliflora@users.noreply.github.com>
2021-02-16 21:08:43 +0100

    Add files via upload

--- evolver_base.c
+++ evolver_base.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <math.h>
 #include <string.h>
+#include "RKF78.h"
 
 /*DEBUGGING:  
 gcc evolver_base.c RKF78-2.2.c/RKF78.c -o evo -lm -O3 -fsanitize=address -static-libasan -g -Wall && time ./evo
@@ -11,13 +12,18 @@ gcc evolver_base.c RKF78-2.2.c/RKF78.c -o evo -lm -O3 -fsanitize=address -static
 #define IC_GENES_NUMBER 3
 #define PARAMETERS_GENES_NUMBER 11
 
-#define CoreModelDIM 8
+#define CORE_MODEL_DIM 8
 #define HMAX 1.0
 #define HMIN 1.e-3
 #define RKTOL 1.e-5
 
-#define n_days 101 // Number of days in time series
-#define n_vars 5   // Number of variables in time series
+#define MAXDOUBLE 1.7976931348623157e+308
+
+#define N_DAYS 101 // Number of days in time series
+#define N_VARS 5   // Number of variables in time series
+
+#define POPSIZE 2000 // Lo que recomienda Alseda
+
 
 //antidiscretising functions
 #define crom2IC(c) (((double) c)/1000)
@@ -31,16 +37,21 @@ gcc evolver_base.c RKF78-2.2.c/RKF78.c -o evo -lm -O3 -fsanitize=address -static
 #define Par2LSDiscPar(c) (c * 1024U)
 
 typedef struct {
-	unsigned long IC[IC_GENES_NUMBER];
-	unsigned long Pars[PARAMETERS_GENES_NUMBER];
-	double DeltaPars[PARAMETERS_GENES_NUMBER];
+	unsigned long IC[IC_GENES_NUMBER];          
+	unsigned long Pars[PARAMETERS_GENES_NUMBER]; 
+	double DeltaPars[PARAMETERS_GENES_NUMBER];  
 	double fitness;
 } individual;
 
 typedef struct {
-	unsigned N_Days;
-	float Data_Time_Series[n_days][n_vars];
+	double beta, phi, epsI, epsY, sigma, gamma1, gamma2, kappa, p, alpha, delta;
 	unsigned PopSize;
+} ODE_Parameters;
+
+typedef struct {
+        double PopSize;
+	unsigned n_days;
+	double Data_Time_Series[N_DAYS][N_VARS];	
 } DataForFitting;
 
 //-----------------------------------
@@ -97,21 +108,6 @@ void encode(individual *ind){
 	ind->Pars[7]  = Par2LSDiscPar(ind->DeltaPars[7]);
 }
 
-void decode(individual *ind){
-	ind->DeltaPars[0]  = crom2HSPar(ind->Pars[0]);
-	ind->DeltaPars[9]  = crom2HSPar(ind->Pars[9]);
-	ind->DeltaPars[10] = crom2HSPar(ind->Pars[10]);
-	ind->DeltaPars[1]  = crom2Par  (ind->Pars[1]);
-	ind->DeltaPars[4]  = crom2Par  (ind->Pars[4]);
-	ind->DeltaPars[5]  = crom2Par  (ind->Pars[5]);
-	ind->DeltaPars[6]  = crom2Par  (ind->Pars[6]);
-	ind->DeltaPars[8]  = crom2Par  (ind->Pars[8]);
-	ind->DeltaPars[2]  = crom2LSPar(ind->Pars[2]);
-	ind->DeltaPars[3]  = crom2LSPar(ind->Pars[3]);
-	ind->DeltaPars[7]  = crom2LSPar(ind->Pars[7]);
-}
-
-
 void indiv_init(individual *ind){
 	for (int i = 0; i < PARAMETERS_GENES_NUMBER; ++i){
 		ind->DeltaPars[i] = uniform();
@@ -149,16 +145,133 @@ void mutate2(unsigned int *a, double mu, unsigned int ndigits){
 }
 
 //-----------------------------------
+void CoreModel(double t, double *x, unsigned CoreModelDIM, double *der, void *Params){
+	ODE_Parameters *par = (ODE_Parameters *) Params; // To simplify the usage of Params (void pointer)
+	
+	double sigmae   = par -> sigma * x[1],
+	       gamma1i1 = par -> gamma1 * x[2],
+	       kappaA   = par -> kappa*x[3],
+	       alphai2  = par -> alpha*x[5];
 
+	der[0] = par -> phi*x[2] + x[3] + (1-par -> epsI)*(x[4]+x[5]) + (1-par -> epsY)*x[6];
+	der[0] = - par -> beta * (x[0] * der[0])/par -> PopSize;
+	der[1] = - der[0] - sigmae;
+	der[2] = sigmae - gamma1i1;
+	der[3] = (1-par -> p)*gamma1i1 - kappaA - par -> gamma2*x[3] ;
+	der[4] = kappaA - par -> gamma2*x[4];
+	der[5] = par -> p*gamma1i1 - par -> gamma2*x[5] - alphai2;
+	der[6] = alphai2 - (par -> gamma2+par -> delta)*x[6];
+	der[7] = par -> gamma2*(x[3] + x[4] + x[5] + x[6]);
+}
 
+int GeneratePredictionFromIndividual(double *xt, void *ODE_pars, DataForFitting *Pred) {
+	register unsigned ndays;
+	double t = 0.0, err, h = 1.e-3;
+	for (ndays=1; ndays <= Pred -> n_days; ndays++) { 
+		int status;
+		while(t+h < ndays) {
+			status = RKF78Sys(&t, xt, CORE_MODEL_DIM, &h, &err, HMIN, HMAX, RKTOL, ODE_pars, CoreModel);
+			if(status) return status;
+		}
+		h = ndays - t;
+		status = RKF78Sys(&t, xt, CORE_MODEL_DIM, &h, &err, HMIN, HMAX, RKTOL, ODE_pars, CoreModel);
+		if(status) return status;
+		Pred -> Data_Time_Series[ndays][0] = xt[4];
+		Pred -> Data_Time_Series[ndays][1] = xt[5];
+		Pred -> Data_Time_Series[ndays][2] = xt[6];
+		Pred -> Data_Time_Series[ndays][3] = xt[7];
+		Pred -> Data_Time_Series[ndays][4] = Pred -> PopSize - (xt[0]+xt[1]+xt[2]+xt[3]+xt[4]+xt[5]+xt[6]+xt[7]);
+	}
+	return 0;
+}
+
+double Parameters2norm(double *parameters, double *x0, void *TheData_void){
+      register unsigned ndays;
+      DataForFitting *TheData = (DataForFitting *) TheData_void;
+      double t=0.0, err, h=1.e-3, norm=0.0;
+      ODE_Parameters ODE_pars = { parameters[0], parameters[1], parameters[2], parameters[3],
+                                  parameters[4], parameters[5], parameters[6], parameters[7],
+                                  parameters[8], parameters[9], parameters[10], TheData->PopSize };
+      double xt[CORE_MODEL_DIM]={x0[0], x0[1], x0[2], x0[3], x0[4], x0[5], x0[6], x0[7] };
+      
+      for (ndays=1; ndays<=TheData->n_days; ndays++) {
+          int status;
+          while (t+h < ndays) {
+                status=RKF78Sys(&t, xt, CORE_MODEL_DIM, &h, &err, HMIN, HMAX, RKTOL, &ODE_pars, CoreModel);
+                if (status) return MAXDOUBLE;
+          }
+          h=ndays-t;
+          status=RKF78Sys(&t, xt, CORE_MODEL_DIM, &h, &err, HMIN, HMAX, RKTOL, &ODE_pars, CoreModel);
+          if (status) return MAXDOUBLE;
+          
+          double d0=xt[4]-TheData -> Data_Time_Series[ndays][0];
+          double d1=xt[5]-TheData -> Data_Time_Series[ndays][1];
+          double d2=xt[6]-TheData -> Data_Time_Series[ndays][2];
+          double d3=xt[7]-TheData -> Data_Time_Series[ndays][3];
+          double d4=TheData -> PopSize - (xt[0]+xt[1]+xt[2]+xt[3]+xt[4]+xt[5]+xt[6]+xt[7]) - TheData -> Data_Time_Series[ndays][4];
+          norm += ndays * (d0*d0+d1*d1+d2*d2+d3*d3+d4*d4);
+      }
+      return norm;      
+}
+
+void CoreModelVersusDataQuadraticError(individual *ind, void *TheData) {
+	DataForFitting *TDfF = (DataForFitting *) TheData;
+	DataForFitting ThePrediction = { 
+		TDfF -> PopSize, 
+		TDfF -> n_days, {
+		{ TDfF -> Data_Time_Series[0][0], 
+		  TDfF -> Data_Time_Series[0][1],
+		  TDfF -> Data_Time_Series[0][2],
+		  TDfF -> Data_Time_Series[0][3],
+		  TDfF -> Data_Time_Series[0][4]
+		}}
+	};
+
+	double xt[CORE_MODEL_DIM] = { 
+		TDfF -> PopSize, 
+		crom2IC(ind -> IC[0]), 
+		crom2IC(ind -> IC[1]),
+		crom2IC(ind -> IC[2]), 
+		TDfF -> Data_Time_Series[0][0],
+		TDfF -> Data_Time_Series[0][1], 
+		TDfF -> Data_Time_Series[0][2],
+		TDfF -> Data_Time_Series[0][3] 
+	};
+
+	xt[0] -= (xt[1]+xt[2]+xt[3]+xt[4]+xt[5]+xt[6]);
+
+	ODE_Parameters ODE_pars = { 
+		ind -> DeltaPars[0], 
+		ind -> DeltaPars[1],
+		ind -> DeltaPars[2], 
+		ind -> DeltaPars[3],
+		ind -> DeltaPars[4], 
+		ind -> DeltaPars[5],
+		ind -> DeltaPars[6], 
+		ind -> DeltaPars[7],
+		ind -> DeltaPars[8], 
+		ind -> DeltaPars[9],
+		ind -> DeltaPars[10], 
+		TDfF -> PopSize 
+	};
+
+	if(GeneratePredictionFromIndividual(xt, &ODE_pars, &ThePrediction)) {
+		ind -> fitness = MAXDOUBLE;
+		return;
+	};
+	ind -> fitness = Parameters2norm(ind->DeltaPars, xt, &ThePrediction);
+}
+
+//-----------------------------------
 
 int main(int argc, char const *argv[])
 {
 	seedinit();	
-	int PopSize = 1000000;
+	//int PopSize = 2000;
 
-	float RD[n_days][n_vars] = {
-		{1.000, 1.000, 0.000, 0.000, 0.000},	
+     /* Real Data (DataForFittint, TheData_void) */
+	float RD[N_DAYS][N_VARS] = {
+	 	{1.000, 1.000, 0.000, 0.000, 0.000},	
 		{1.841, 1.253, 0.056, 0.348, 0.000},
 		{2.285, 1.607, 0.123, 0.733, 0.002},
 		{2.571, 2.041, 0.203, 1.169, 0.004},
@@ -260,23 +373,33 @@ int main(int argc, char const *argv[])
 		{23002.296, 32448.131, 9788.183, 71455.071, 1629.977},
 		{24711.260, 34688.480, 10562.489, 77627.195, 1777.437}
 	};
-
-	DataForFitting realData;
-
-	realData.PopSize = PopSize;
-	realData.N_Days = n_days;
-	memcpy(realData.Data_Time_Series, RD, sizeof(float)*n_days*n_vars);
-	printf("%f\n", realData.Data_Time_Series[2][1]);
-
-	individual *population;
-	population = (individual *) malloc(sizeof(individual) * PopSize);
-
-	for (int i = 0; i < PopSize; ++i){ indiv_init(&population[i]); }
 	
+	DataForFitting realData;
+        realData.PopSize = 1000000;
+	memcpy(realData.Data_Time_Series, RD, sizeof(float)*N_DAYS*N_VARS);
+	//printf("%f\n", realData.Data_Time_Series[2][1]);
+	
+     /* Initial Population */
+        individual *population;
+	population = (individual *) malloc(sizeof(individual) * POPSIZE);
 
-
-
-
+	for (int i = 0; i < POPSIZE; ++i){
+	     indiv_init(&population[i]); 	     
+	}
+	//printf("%f\n", population -> fitness);
+     
+        individual p;
+	indiv_init(&p);
+	//printind(p);
+     
+        CoreModelVersusDataQuadraticError(&p, &realData);
+        printf("%.50f\n", p.fitness);
+     /* Compute Fitness 
+        for (int i=0; i < POPSIZE; ++i){
+	     CoreModelVersusDataQuadraticError(&population[i], &realData);
+	}
+	printf("%f\n", population -> fitness);
+     */
 
 /*	//MUTATION TEST
 	unsigned int ndigits = 20, chr;
